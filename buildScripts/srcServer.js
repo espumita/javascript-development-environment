fb555b3a6a499184211bab36eaa2494e6f50fd6d
espumita <david_el_125@hotmail.com>
2017-04-13 20:57:34 +0100

    Simple fetch data

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -13,10 +13,19 @@ app.use(require('webpack-dev-middleware')(compiler, {
     publicPath: config.output.publicPath
 }));
 
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
+
+app.get('/users', (req, res) => {
+    res.json([
+        {"id": 1,"firstName": "Bob", "email": "Bob@gmail.com"},
+        {"id": 2,"firstName": "Mike", "email": "Mike@gmail.com"},
+        {"id": 3,"firstName": "Tina", "email": "Tina@gmail.com"}
+    ]);
+});
+
+
 
 app.listen(port, err => {
     if (err){
